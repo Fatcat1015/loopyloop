91fd19faa9459057daebe6373368bbc4ecb1b6c3
Yunfei Gao <yg2251@nyu.edu>
2022-10-24 17:35:31 -0400

    Final fixes

--- Assets/scripts/PlayerMovementFPS.cs
+++ Assets/scripts/PlayerMovementFPS.cs
@@ -34,23 +34,25 @@ public class PlayerMovementFPS : MonoBehaviour
 
         if (isGrounded && velocity.y < 0)
         {
-            velocity.y = -0.5f;
+            velocity.y = -2f;
         }
-        float x = Input.GetAxis("Horizontal");
-        float z = Input.GetAxis("Vertical");
 
-        Vector3 move = transform.right * x + transform.forward * z;
+            float x = Input.GetAxis("Horizontal");
+            float z = Input.GetAxis("Vertical");
 
-        cc.Move(move * speed * Time.deltaTime);
+            Vector3 move = transform.right * x + transform.forward * z;
+
+            cc.Move(move * speed * Time.deltaTime);
+        
+        
 
         if (Input.GetButtonDown("Jump") && isGrounded)
         {
-            velocity.y = Mathf.Sqrt(jumpHeight * gravity * -1);
+            velocity.y = Mathf.Sqrt(jumpHeight * gravity * -5f);
         }
 
-        velocity.y += gravity * Time.deltaTime;
-
-        cc.Move(velocity * gravity * -0.001f);
+        velocity.y += gravity * Time.deltaTime * 2f;
+        cc.Move(velocity * Time.deltaTime);
     }
 
     void OnDrawGizmos()
